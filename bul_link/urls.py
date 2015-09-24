decfa951e7dcfc816b5fedc83834c1bd2cf6a4f4
birkin <birkin.diana@gmail.com>
2015-09-24 10:08:33 -0400

    updates url imports for newer django.

--- bul_link/urls.py
+++ bul_link/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.views.generic import TemplateView
-from django.conf.urls.defaults import *
+# from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns, url
 
 from views import ResolveView
 from app_settings import PERMALINK_PREFIX
 
-urlpatterns = patterns('', 
+urlpatterns = patterns('',
     #Handle permalinks or OpenURL lookups
     url(r'^get/%s(?P<tiny>.*)/$' % PERMALINK_PREFIX,
             ResolveView.as_view(),
