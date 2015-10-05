ddccc959ea1b3e44e1db1aecaed16ad64f0bc627
birkin <birkin.diana@gmail.com>
2015-10-05 15:43:09 -0400

    adds logging.

--- delivery/middleware.py
+++ delivery/middleware.py
@@ -45,6 +45,8 @@ class DeliveryShib(ShibbolethRemoteUserMiddleware):
         """
         #Catch any occurrences where the profile doesn't yet exist.
         try:
+            alog.debug( 'type(user), `%s`' % type(user) )
+            alog.debug( 'user.__dict__, `%s`' % user.__dict__ )
             profile = user.profile
         except ObjectDoesNotExist:
             profile = LibraryProfile.objects.create(user=user)
