a1548b4b74b13e47b8690ad332d0b957d507b0fa
birkin <birkin.diana@gmail.com>
2015-10-08 13:29:36 -0400

    eliminates default cache timeout.

--- bul_link/app_settings.py
+++ bul_link/app_settings.py
@@ -7,16 +7,16 @@ if SERSOL_KEY is None:
      raise ImproperlyConfigured('The link360 app requires a valid SERSOL_KEY')
 
 SERSOL_TIMEOUT = getattr(settings, 'BUL_LINK_SERSOL_TIMEOUT', 30)
- 
+
 PERMALINK_PREFIX = getattr(settings, 'BUL_LINK_PERMALINK_PREFIX', 'bL')
 
-CACHE_TIMEOUT = getattr(settings, 'BUL_LINK_CACHE_TIMEOUT', 60)
+CACHE_TIMEOUT = getattr(settings, 'BUL_LINK_CACHE_TIMEOUT', 0)
 
 #These keys will be skipped when creating the cache key for resolved urls.
 QUERY_SKIP_KEYS = ['csrfmiddlewaretoken', #redirects from forms
                     'rfr_id',
                     'paramdict',
-                    'sid', 
+                    'sid',
                     'output' #internal output format specifier
                     ]
 
