46693098f1272ae8b4c21dd8de80978e8c956682
birkin <birkin.diana@gmail.com>
2015-10-14 09:33:38 -0400

    redirects root url.

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,18 @@
-# from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 admin.autodiscover()
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^user/', include('shibboleth.urls', namespace='shibboleth')),
-    url(r'^borrow/', include('delivery.urls', namespace='delivery')),
-    # url(r'^', include('findit.urls', namespace='findit')),
-    url(r'^find/', include('findit.urls', namespace='findit')),
-    #These are here just to register the namespace.  All links will be handled
-    #by the above.
-    # url(r'', include('bul_link.urls', namespace='bul_link')),
+    url( r'^admin/', include(admin.site.urls) ),
+    url( r'^user/', include('shibboleth.urls', namespace='shibboleth') ),
+    url( r'^borrow/', include('delivery.urls', namespace='delivery') ),
+    url( r'^find/', include('findit.urls', namespace='findit') ),
+    url( r'^$',  RedirectView.as_view(url='find/') ),
     ]
 
-# urlpatterns = patterns('',
-#     url(r'^admin/', include(admin.site.urls)),
-#     url(r'^user/', include('shibboleth.urls', namespace='shibboleth')),
-#     url(r'^borrow/', include('delivery.urls', namespace='delivery')),
-#     url(r'', include('findit.urls', namespace='findit')),
-#     #These are here just to register the namespace.  All links will be handled
-#     #by the above.
-#     url(r'', include('bul_link.urls', namespace='bul_link')),
-# )
-
-# from django.conf.urls.defaults import *
-#For 500 errors
-handler500 = 'findit.views.server_error'
+## for 500 errors
 handler500 = 'findit.views.server_error'
