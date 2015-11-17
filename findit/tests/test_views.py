b8b2c3e55a5dd3865dcc6940f1f81215ab10bbe3
birkin <birkin.diana@gmail.com>
2015-11-17 09:54:32 -0500

    adds test for main index page.

--- findit/tests/test_views.py
+++ findit/tests/test_views.py
@@ -3,16 +3,11 @@
 from __future__ import unicode_literals
 
 import logging, pprint
-# from pprint import pprint
 from django.conf import settings
-from django.utils import unittest
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.test.client import RequestFactory
-from django.test.client import Client
 from django.utils.log import dictConfig
-
-from findit import views
-from findit import utils
+from findit import utils, views
 
 settings.BUL_LINK_CACHE_TIMEOUT = 0
 settings.CACHES = {}
@@ -21,11 +16,24 @@ dictConfig(settings.LOGGING)
 log = logging.getLogger('access')
 
 
+class IndexPageLinksTest( TestCase ):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get( '/find/' )  # project root part of url is assumed
+        self.assertEqual( 200, response.status_code )
+        self.assertEqual( True, 'class="intro">Examples' in response.content )
+
+    ## end class IndexPageLinksTest
+
+
 #Non-book, non-article item
 #http://www.worldcat.org/title/einstein-on-the-beach/oclc/29050194
 #coins
 #http://generator.ocoins.info/?sitePage=info/journal.html
-class PmidResolverTest(unittest.TestCase):
+class PmidResolverTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
@@ -63,7 +71,7 @@ class PmidResolverTest(unittest.TestCase):
         self.assertIn('0008-350X', context['citation']['issn']['print'])
 
 
-class PrintResolverTest(unittest.TestCase):
+class PrintResolverTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
     def test_print_holding(self):
@@ -76,7 +84,7 @@ class PrintResolverTest(unittest.TestCase):
         self.assertTrue(context['link_groups'][0]['holdingData']['providerName'],
                         'Library Specific Holdings')
 
-class EasyBorrowResolverTest(unittest.TestCase):
+class EasyBorrowResolverTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
@@ -108,7 +116,7 @@ class EasyBorrowResolverTest(unittest.TestCase):
         self.assertTrue(eb_dict['isbn'], '9780394565279')
         self.assertTrue(eb_dict['author'], 'Russo, Richard')
 
-class EbookResolverTest(unittest.TestCase):
+class EbookResolverTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
@@ -126,7 +134,7 @@ class EbookResolverTest(unittest.TestCase):
         self.assertTrue('Ebrary Academic Complete Subscription Collection',
                         lg[0]['holdingData']['databaseName'])
 
-class ConferenceReportResolverTest(unittest.TestCase):
+class ConferenceReportResolverTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
     def test_econference_report(self):
@@ -137,7 +145,7 @@ class ConferenceReportResolverTest(unittest.TestCase):
         self.assertTrue('Electrical and Computer Engineering (CCECE), 2011 24th Canadian Conference on', citation['source'])
         self.assertTrue('Islam', citation['creatorLast'])
 
-class PublicationView(unittest.TestCase):
+class PublicationView(TestCase):
 
     def test_publication_view_redirect(self):
         ourl = '?sid=FirstSearch%3AWorldCat&genre=journal&issn=0017-811X&eissn=2161-976X&title=Harvard+law+review.&date=1887&id=doi%3A&pid=<accession+number>1751808<%2Faccession+number><fssessid>0<%2Ffssessid>&url_ver=Z39.88-2004&rfr_id=info%3Asid%2Ffirstsearch.oclc.org%3AWorldCat&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&req_dat=<sessionid>0<%2Fsessionid>&rfe_dat=<accessionnumber>1751808<%2Faccessionnumber>&rft_id=info%3Aoclcnum%2F1751808&rft_id=urn%3AISSN%3A0017-811X&rft.jtitle=Harvard+law+review.&rft.issn=0017-811X&rft.eissn=2161-976X&rft.aucorp=Harvard+Law+Review+Publishing+Association.%3BHarvard+Law+Review+Association.&rft.place=Cambridge++Mass.&rft.pub=Harvard+Law+Review+Pub.+Association&rft.genre=journal&checksum=059306b04e1938ee38f852a498bea79e&title=Brown%20University&linktype=openurl&detail=RBN'
