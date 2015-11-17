603dedea8e1468a2b7977fae26183bc460935c35
birkin <birkin.diana@gmail.com>
2015-11-17 09:13:26 -0500

    updates tests.

--- findit/tests/test_views.py
+++ findit/tests/test_views.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
 
-from pprint import pprint
+from __future__ import unicode_literals
+
+import logging, pprint
+# from pprint import pprint
 from django.conf import settings
 from django.utils import unittest
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.test.client import Client
+from django.utils.log import dictConfig
 
 from findit import views
 from findit import utils
@@ -12,20 +17,42 @@ from findit import utils
 settings.BUL_LINK_CACHE_TIMEOUT = 0
 settings.CACHES = {}
 
+dictConfig(settings.LOGGING)
+log = logging.getLogger('access')
+
+
 #Non-book, non-article item
 #http://www.worldcat.org/title/einstein-on-the-beach/oclc/29050194
 #coins
 #http://generator.ocoins.info/?sitePage=info/journal.html
-class PmidResolverTest(unittest.TestCase):   
+class PmidResolverTest(unittest.TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-        
+
+    # def test_pmid(self):
+    #     request = self.factory.get('?pmid=21221393')
+    #     response = views.ResolveView(request=request)
+    #     #Call get context data to simulate browser hit.
+    #     context = response.get_context_data()
+    #     #Verify returned links
+    #     names = [link['holdingData']['providerName'] for link in context['link_groups']]
+    #     self.assertIn("National Library of Medicine", names)
+    #     #Verify citation.
+    #     citation = context['citation']
+    #     self.assertTrue(citation['source'], 'Canadian family physician')
+    #     self.assertTrue(citation['volume'], '38')
+    #     self.assertTrue(citation['pmid'], '21221393')
+    #     self.assertIn('0008-350X', context['citation']['issn']['print'])
+
     def test_pmid(self):
         request = self.factory.get('?pmid=21221393')
-        response = views.ResolveView(request=request)
-        #Call get context data to simulate browser hit. 
+        # response = views.ResolveView(request=request)
+        response = views.base_resolver( request=request )
+        log.debug( 'response, ```%s```' % pprint.pformat(response.__dict__) )
+
+        ## Call get context data to simulate browser hit.
         context = response.get_context_data()
-        #Verify returned links
+        ## Verify returned links
         names = [link['holdingData']['providerName'] for link in context['link_groups']]
         self.assertIn("National Library of Medicine", names)
         #Verify citation.
@@ -34,11 +61,11 @@ class PmidResolverTest(unittest.TestCase):
         self.assertTrue(citation['volume'], '38')
         self.assertTrue(citation['pmid'], '21221393')
         self.assertIn('0008-350X', context['citation']['issn']['print'])
-        
 
-class PrintResolverTest(unittest.TestCase):   
+
+class PrintResolverTest(unittest.TestCase):
     def setUp(self):
-        self.factory = RequestFactory()   
+        self.factory = RequestFactory()
     def test_print_holding(self):
         request = self.factory.get('?rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft.spage=80&rft.au=Churchland%2C%20PS&rft.aulast=Churchland&rft.format=journal&rft.date=1983&rft.aufirst=PS&rft.volume=64&rft.eissn=1468-0114&rft.atitle=Consciousness%3A%20The%20transmutation%20of%20a%20concept&rft.jtitle=Pacific%20philosophical%20quarterly&rft.issn=0279-0750&rft.genre=article&url_ver=Z39.88-2004&rfr_id=info:sid/libx%3Abrown')
         response = views.ResolveView(request=request)
@@ -51,8 +78,8 @@ class PrintResolverTest(unittest.TestCase):
 
 class EasyBorrowResolverTest(unittest.TestCase):
     def setUp(self):
-        self.factory = RequestFactory()  
-        
+        self.factory = RequestFactory()
+
     def test_pass_to_easy_borrow(self):
         """
         Book requests (like this) should be handed off to easyBorrow.
@@ -65,7 +92,7 @@ class EasyBorrowResolverTest(unittest.TestCase):
         self.assertTrue(content.rfind('ezborrow') > -1)
         #also make sure the OCLC number was passed on
         self.assertTrue(content.rfind('17803510') > -1)
-        
+
     def test_easy_borrow_handoff(self):
         from findit.utils import BulSerSol
         import urlparse
@@ -81,10 +108,10 @@ class EasyBorrowResolverTest(unittest.TestCase):
         self.assertTrue(eb_dict['isbn'], '9780394565279')
         self.assertTrue(eb_dict['author'], 'Russo, Richard')
 
-class EbookResolverTest(unittest.TestCase):   
+class EbookResolverTest(unittest.TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-                  
+
     def test_ebook(self):
         o = '?rft.pub=Yale+University+Press&rft.aulast=Dyson&rft.isbn=9780300110975&rft.isbn=9780300110975&rft.author=Dyson%2C+Stephen+L&rft.btitle=In+pursuit+of+ancient+pasts+%3A+a+history+of+classical+archaeology+in+the+nineteenth+and+twentieth+centuries&rft.btitle=In+pursuit+of+ancient+pasts+%3A+a+history+of+classical+archaeology+in+the+nineteenth+and+twentieth+centuries&rft.aufirst=Stephen&rft.place=New+Haven&rft.date=2006&rft.auinitm=L&url_ver=Z39.88-2004&version=1.0&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Abook&rft.genre=book&rfe_dat=%3Caccessionnumber%3E70062939%3C%2Faccessionnumber%3E'
         request = self.factory.get(o)
@@ -99,9 +126,9 @@ class EbookResolverTest(unittest.TestCase):
         self.assertTrue('Ebrary Academic Complete Subscription Collection',
                         lg[0]['holdingData']['databaseName'])
 
-class ConferenceReportResolverTest(unittest.TestCase):   
+class ConferenceReportResolverTest(unittest.TestCase):
     def setUp(self):
-        self.factory = RequestFactory()          
+        self.factory = RequestFactory()
     def test_econference_report(self):
         request = self.factory.get('?id=10.1109/CCECE.2011.6030651')
         response = views.ResolveView(request=request)
@@ -109,17 +136,17 @@ class ConferenceReportResolverTest(unittest.TestCase):
         citation = context['citation']
         self.assertTrue('Electrical and Computer Engineering (CCECE), 2011 24th Canadian Conference on', citation['source'])
         self.assertTrue('Islam', citation['creatorLast'])
-        
+
 class PublicationView(unittest.TestCase):
-        
+
     def test_publication_view_redirect(self):
         ourl = '?sid=FirstSearch%3AWorldCat&genre=journal&issn=0017-811X&eissn=2161-976X&title=Harvard+law+review.&date=1887&id=doi%3A&pid=<accession+number>1751808<%2Faccession+number><fssessid>0<%2Ffssessid>&url_ver=Z39.88-2004&rfr_id=info%3Asid%2Ffirstsearch.oclc.org%3AWorldCat&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&req_dat=<sessionid>0<%2Fsessionid>&rfe_dat=<accessionnumber>1751808<%2Faccessionnumber>&rft_id=info%3Aoclcnum%2F1751808&rft_id=urn%3AISSN%3A0017-811X&rft.jtitle=Harvard+law+review.&rft.issn=0017-811X&rft.eissn=2161-976X&rft.aucorp=Harvard+Law+Review+Publishing+Association.%3BHarvard+Law+Review+Association.&rft.place=Cambridge++Mass.&rft.pub=Harvard+Law+Review+Pub.+Association&rft.genre=journal&checksum=059306b04e1938ee38f852a498bea79e&title=Brown%20University&linktype=openurl&detail=RBN'
         #response = views.Reso
         client = Client()
         response = client.get('/%s' % ourl)
         self.assertEqual(response.status_code, 302)
-    
-        
-        
-        
-        
+
+
+
+
+
