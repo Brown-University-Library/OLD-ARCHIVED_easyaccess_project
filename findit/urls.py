c6f37dce78c5368c7b83679879034680ed93bcc0
birkin <birkin.diana@gmail.com>
2016-02-15 12:19:23 -0500

    comments out url entries.

--- findit/urls.py
+++ findit/urls.py
@@ -2,67 +2,29 @@
 
 from __future__ import unicode_literals
 
+from app_settings import PERMALINK_PREFIX
 from django.conf import settings
 from django.views.generic import TemplateView
-# from django.conf.urls.defaults import *
 from django.conf.urls import include, patterns, url
-
-# from views import Resolver, CitationFormView, RequestView,\
-#                   UserView, MicrosoftView, JstorView, SummonView
-
-from views import CitationFormView, RequestView, UserView, MicrosoftView, JstorView, SummonView
-
-from app_settings import PERMALINK_PREFIX
+from views import CitationFormView, JstorView, MicrosoftView, RequestView, SummonView, UserView
 
 
 urlpatterns = patterns('',
 
     url( r'^citation-form/$',  CitationFormView.as_view(), name='citation-form-view' ),
 
-    url( r'^request/(?P<resource>[0-9]+)/$',  RequestView.as_view(), name='request-view' ),
+    # url( r'^request/(?P<resource>[0-9]+)/$',  RequestView.as_view(), name='request-view' )  # might be needed for illiad requests, but I'd think they would go to /borrow
 
-    url( r'^mas/$',  MicrosoftView.as_view(), name='microsoft-view' ),
+    # url( r'^mas/$',  MicrosoftView.as_view(), name='microsoft-view' ),
 
-    url( r'^jstor/$',  JstorView.as_view(), name='jstor-view' ),
+    # url( r'^jstor/$',  JstorView.as_view(), name='jstor-view' ),
 
-    url( r'^user/$',  UserView.as_view(), name='user-view' ),
+    # url( r'^user/$',  UserView.as_view(), name='user-view' ),
 
     #Handle permalinks or OpenURL lookups
-    # url( r'^get/%s(?P<tiny>.*)/$' % PERMALINK_PREFIX,  Resolver.as_view(), name='permalink-view' ),
     url( r'^get/%s(?P<tiny>.*)/$' % PERMALINK_PREFIX,  'findit.views.tiny_resolver', name=u'permalink_url' ),
-
     url( r'^get/(?P<id_type>pmid|doi)/(?P<id_value>.*)/$',  SummonView.as_view(), name='summon-view' ),
 
-    # url( r'^$',  Resolver.as_view(), name='resolver-view' ),
     url( r'^$',  'findit.views.findit_base_resolver', name=u'findit_base_resolver_url' ),
 
     )
-
-# urlpatterns = patterns('',
-#     #citation form
-#     url(r'^citation-form/$',
-#         CitationFormView.as_view(),
-#         name='citation-form-view'),
-#     url(r'^request/(?P<resource>[0-9]+)/$',
-#         RequestView.as_view(),
-#         name='request-view'),
-#     url(r'^mas/$',
-#         MicrosoftView.as_view(),
-#         name='microsoft-view'),
-#     url(r'^jstor/$',
-#         JstorView.as_view(),
-#         name='jstor-view'),
-#     url(r'^user/$',
-#         UserView.as_view(),
-#         name='user-view'),
-#     #Handle permalinks or OpenURL lookups
-#     url(r'^get/%s(?P<tiny>.*)/$' % PERMALINK_PREFIX,
-#             Resolver.as_view(),
-#             name='permalink-view'),
-#     url(r'^get/(?P<id_type>pmid|doi)/(?P<id_value>.*)/$',
-#         SummonView.as_view(),
-#         name='summon-view'),
-#     url(r'^',
-#         Resolver.as_view(),
-#         name='resolver-view'),
-# )
