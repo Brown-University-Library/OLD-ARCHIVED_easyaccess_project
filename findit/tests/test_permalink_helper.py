cf76f0dd2e2f1ec375e20c72192c973b82b4aeea
birkin <birkin.diana@gmail.com>
2016-02-19 12:30:32 -0500

    updates permalink tests.

--- findit/tests/test_permalink_helper.py
+++ findit/tests/test_permalink_helper.py
@@ -8,7 +8,7 @@ from django.http import QueryDict
 from django.test import TestCase
 
 from delivery.models import Resource
-from findit.classes.findit_resolver_helper import FinditResolver
+from findit.classes.permalink_helper import Permalink
 from shorturls import baseconv
 
 
@@ -16,11 +16,11 @@ log = logging.getLogger('access')
 TestCase.maxDiff = None
 
 
-class FinditResolverHelperTest( TestCase ):
+class PermalinkHelperTest( TestCase ):
     """ Checks helper functions. """
 
     def setUp(self):
-        self.helper = FinditResolver()
+        self.helper = Permalink()
         self.qdct = QueryDict( '', mutable=True )
 
     def test_make_permalink__resource_not_found(self):
@@ -28,7 +28,7 @@ class FinditResolverHelperTest( TestCase ):
         qstring = 'sid=FirstSearch%3AWorldCat&genre=journal&issn=0017-811X&eissn=2161-976X&title=Harvard+law+review.&date=1887&id=doi%3A&pid=<accession+number>1751808<%2Faccession+number><fssessid>0<%2Ffssessid>&url_ver=Z39.88-2004&rfr_id=info%3Asid%2Ffirstsearch.oclc.org%3AWorldCat&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&req_dat=<sessionid>0<%2Fsessionid>&rfe_dat=<accessionnumber>1751808<%2Faccessionnumber>&rft_id=info%3Aoclcnum%2F1751808&rft_id=urn%3AISSN%3A0017-811X&rft.jtitle=Harvard+law+review.&rft.issn=0017-811X&rft.eissn=2161-976X&rft.aucorp=Harvard+Law+Review+Publishing+Association.%3BHarvard+Law+Review+Association.&rft.place=Cambridge++Mass.&rft.pub=Harvard+Law+Review+Pub.+Association&rft.genre=journal&checksum=059306b04e1938ee38f852a498bea79e&title=Brown%20University&linktype=openurl&detail=RBN'
         qdct = { 'rfr_id': 'info:sid/firstsearch.oclc.org:WorldCat' }
         self.qdct.update( qdct )
-        data_dct = self.helper.make_permalink( qdct.get('rfr_id',''), qstring, scheme='https', host='the_host' )
+        data_dct = self.helper.make_permalink( referrer=qdct.get('rfr_id',''), qstring=qstring, scheme='https', host='the_host', path='/easyaccess/find/' )
         self.assertEqual(
             'https://the_host/easyaccess/find/permalink/B/', data_dct['permalink'] )
         self.assertEqual(
@@ -55,7 +55,7 @@ class FinditResolverHelperTest( TestCase ):
         rsc2.query = good_qstring
         rsc2.referrer = good_referrer
         rsc2.save()
-        data_dct = self.helper.make_permalink( good_referrer, good_qstring, scheme='https', host='the_host' )
+        data_dct = self.helper.make_permalink( referrer=good_referrer, qstring=good_qstring, scheme='https', host='the_host', path='/easyaccess/find/' )
         self.assertEqual(
             'https://the_host/easyaccess/find/permalink/pnfq/', data_dct['permalink'] )
         self.assertEqual(
