31d30fc506e286155ce1b072c0a1f66c1fa86434
birkin <birkin.diana@gmail.com>
2016-03-07 22:19:03 -0500

    updates alternate prep.

--- delivery/views.py
+++ delivery/views.py
@@ -83,10 +83,15 @@ def availability( request ):
     for item in jdct['items']:
         if item['is_available'] is True:
             oclc_num_callnumber = item['callnumber']
+            # log.debug( 'oclc_num_callnumber, ```{}```'.format(oclc_num_callnumber) )
+            match_check = False
             for holding in isbn_holdings:
-                if oclc_num_callnumber != holding['callnumber']:
-                    oclc_holdings.append( {'callnumber': item['callnumber'], 'location': item['location'], 'status': item['availability']} )
+                log.debug( 'holding, ```{}```'.format(holding) )
+                if oclc_num_callnumber == holding['callnumber']:
+                    match_check = True
                     break
+            if match_check is False:
+                oclc_holdings.append( {'callnumber': item['callnumber'], 'location': item['location'], 'status': item['availability']} )
     for holding in oclc_holdings:
         isbn_holdings.append( holding )
     available_holdings = isbn_holdings
