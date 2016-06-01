6407cac0d7bfb05753ca0c5fdfefb6b1d95efebf
birkin <birkin.diana@gmail.com>
2016-06-01 07:25:31 -0400

    updates variable reference.

--- article_request_app/views.py
+++ article_request_app/views.py
@@ -73,7 +73,7 @@ def shib_login( request ):
     else:
         encoded_login_handler_url = urlquote( login_handler_url )
         redirect_url = '{shib_login}?target={encoded_login_handler_url}'.format(
-            shib_login=app_settings.SHIB_LOGIN_URL, encoded_login_handler_url=encoded_login_handler_url )
+            shib_login=settings_app.SHIB_LOGIN_URL, encoded_login_handler_url=encoded_login_handler_url )
         log.debug( 'redirect_url to shib-sp-login, ```{}```'.format(redirect_url) )
         return HttpResponseRedirect( redirect_url )
 
