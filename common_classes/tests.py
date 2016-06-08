adc2b5e2b33db7d235a845a8ef60b20b2f19547f
birkin <birkin.diana@gmail.com>
2016-06-08 10:36:51 -0400

    implements blocked-user tests.

--- common_classes/tests.py
+++ common_classes/tests.py
@@ -36,7 +36,7 @@ class IlliadHelperTest( TestCase ):
         self.assertEqual( True, ok )
 
     def test_login__good_user(self):
-        """ Good-user login should indicate user is authenticated and registered. """
+        """ Good-user login should show user is authenticated, and is registered. """
         ill_username = settings_app.TEST_ILLIAD_GOOD_USERNAME
         ( illiad_session_instance, ok ) = self.helper.connect( ill_username )
         self.illiad_session_instance = illiad_session_instance
@@ -54,14 +54,42 @@ class IlliadHelperTest( TestCase ):
             ['authenticated', 'new_user', 'registered', 'session_id'],
             sorted(login_dct.keys()) )
         self.assertEqual( True, login_dct['authenticated'])
+        self.assertEqual( False, login_dct['new_user'])
         self.assertEqual( True, login_dct['registered'])
         self.assertEqual( illiad_session_instance.session_id, login_dct['session_id'])
         ## ok check
         self.assertEqual( True, ok )
 
-    # def test_login__blocked_user(self):
-    #     """ Good-user login should indicate user is authenticated and registered. """
-    #     ill_username = settings_app.TEST_ILLIAD_BLOCKED_USERNAME
+    def test_login__blocked_user(self):
+        """ Good-user login should show user is not authenticated, is registered, and is blocked. """
+        ill_username = settings_app.TEST_ILLIAD_BLOCKED_USERNAME
+        ( illiad_session_instance, ok ) = self.helper.connect( ill_username )
+        self.illiad_session_instance = illiad_session_instance
+        ( illiad_session_instance, login_dct, ok ) = self.helper.login( illiad_session_instance )
+        ## instance checks
+        self.assertEqual(
+            ['auth_header', 'blocked_patron', 'cookies', 'header', 'registered', 'session_id', 'url', 'username'],
+            sorted(illiad_session_instance.__dict__.keys()) )
+        self.assertEqual( True, illiad_session_instance.blocked_patron )
+        self.assertEqual( True, illiad_session_instance.registered )
+        self.assertEqual( None, illiad_session_instance.session_id )
+        self.assertEqual( settings_app.TEST_ILLIAD_BLOCKED_USERNAME, illiad_session_instance.username )
+        ## login_dct checks
+        self.assertEqual(
+            ['authenticated', 'blocked', 'new_user', 'session_id'],
+            sorted(login_dct.keys()) )
+        self.assertEqual( False, login_dct['authenticated'])
+        self.assertEqual( True, login_dct['blocked'])
+        self.assertEqual( False, login_dct['new_user'])
+        self.assertEqual( None, login_dct['session_id'])
+        ## ok check
+        self.assertEqual( True, ok )
+
+
+
+    # def test_login__disavowed_user(self):
+    #     """ Good-user login should show user is authenticated, and is registered. """
+    #     ill_username = settings_app.TEST_ILLIAD_DISAVOWED_USERNAME
     #     ( illiad_session_instance, ok ) = self.helper.connect( ill_username )
     #     self.illiad_session_instance = illiad_session_instance
     #     ( illiad_session_instance, login_dct, ok ) = self.helper.login( illiad_session_instance )
@@ -72,16 +100,20 @@ class IlliadHelperTest( TestCase ):
     #     self.assertEqual( False, illiad_session_instance.blocked_patron )
     #     self.assertEqual( True, illiad_session_instance.registered )
     #     self.assertEqual( 11, len(illiad_session_instance.session_id) )
-    #     self.assertEqual( settings_app.TEST_ILLIAD_BLOCKED_USERNAME, illiad_session_instance.username )
+    #     self.assertEqual( settings_app.TEST_ILLIAD_DISAVOWED_USERNAME, illiad_session_instance.username )
     #     ## login_dct checks
     #     self.assertEqual(
     #         ['authenticated', 'new_user', 'registered', 'session_id'],
     #         sorted(login_dct.keys()) )
     #     self.assertEqual( True, login_dct['authenticated'])
+    #     self.assertEqual( False, login_dct['new_user'])
     #     self.assertEqual( True, login_dct['registered'])
     #     self.assertEqual( illiad_session_instance.session_id, login_dct['session_id'])
     #     ## ok check
     #     self.assertEqual( True, ok )
+
+
+
 
     def tearDown(self):
         self.helper.logout_user( self.illiad_session_instance )
