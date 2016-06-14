5e56e47bcd5a4e3e493093dcb595b3ad6857a741
birkin <birkin.diana@gmail.com>
2016-06-14 09:50:10 -0400

    uses new log-id.

--- delivery/classes/process_helper.py
+++ delivery/classes/process_helper.py
@@ -39,12 +39,12 @@ If you believe you should be permitted to use interlibrary-loan services, please
     def check_if_authorized( self, shib_dct ):
         """ Checks whether user is authorized to request book.
             Called by views.process_request() """
-        log.debug( '`{id}` checking authorization'.format(id='coming') )
+        log.debug( '`{id}` checking authorization'.format(id=self.log_id) )
         ( is_authorized, redirect_url, message ) = ( False, reverse('delivery:message_url'), self.denied_permission_message )
         if app_settings.REQUIRED_GROUPER_GROUP in shib_dct.get( 'member_of', '' ):
-            log.debug( '`{id}` user authorized'.format(id='coming') )
+            log.debug( '`{id}` user authorized'.format(id=self.log_id) )
             ( is_authorized, redirect_url, message ) = ( True, '', '' )
-        log.debug( '`{id}` is_authorized, `{auth}`; redirect_url, `{url}`; message, ```{msg}```'.format(id='coming', auth=is_authorized, url=redirect_url, msg=message) )
+        log.debug( '`{id}` is_authorized, `{auth}`; redirect_url, `{url}`; message, ```{msg}```'.format(id=self.log_id, auth=is_authorized, url=redirect_url, msg=message) )
         return ( is_authorized, redirect_url, message )
 
     def save_to_easyborrow( self, shib_dct, bib_dct, querystring ):
