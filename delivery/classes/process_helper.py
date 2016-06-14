18e944cab895bb377003289f9017bf3174cd9d56
birkin <birkin.diana@gmail.com>
2016-06-14 09:15:31 -0400

    adds authZ tests.

--- delivery/classes/process_helper.py
+++ delivery/classes/process_helper.py
@@ -23,8 +23,6 @@ It appears you are not authorized to use interlibrary-loan services, which are f
 If you believe you should be permitted to use interlibrary-loan services, please contact the circulation staff at the Rockefeller Library, or call them at {phone}, or email them at {email}, and they'll help you.
 '''.format( phone=app_settings.PERMISSION_DENIED_PHONE, email=app_settings.PERMISSION_DENIED_EMAIL )
 
-
-
     def check_referrer( self, session_dct, meta_dct ):
         """ Ensures request came from /availability/.
             Called by views.login() """
@@ -44,7 +42,7 @@ If you believe you should be permitted to use interlibrary-loan services, please
             Called by views.process_request() """
         log.debug( '`{id}` checking authorization'.format(id='coming') )
         ( is_authorized, redirect_url, message ) = ( False, reverse('delivery:message_url'), self.denied_permission_message )
-        if app_settings.REQUIRED_GROUPER_GROUP in shib_dct['member_of']:
+        if app_settings.REQUIRED_GROUPER_GROUP in shib_dct.get( 'member_of', '' ):
             log.debug( '`{id}` user authorized'.format(id='coming') )
             ( is_authorized, redirect_url, message ) = ( True, '', '' )
         log.debug( '`{id}` is_authorized, `{auth}`; redirect_url, `{url}`; message, ```{msg}```'.format(id='coming', auth=is_authorized, url=redirect_url, msg=message) )
--- delivery/tests/test_process_helper.py
+++ delivery/tests/test_process_helper.py
@@ -4,8 +4,9 @@
 from __future__ import unicode_literals
 
 import logging, pprint
-from django.test import TestCase
+from delivery import app_settings
 from delivery.classes.process_helper import ProcessViewHelper
+from django.test import TestCase
 
 
 log = logging.getLogger('access')
@@ -20,12 +21,27 @@ class ProcessHelperTest(TestCase):
     def setUp(self):
         self.helper = ProcessViewHelper()
 
+    def test_check_if_authorized__bad_data(self):
+        """ Bad data should return False. """
+        shib_dct = {}
+        ( is_authorized, redirect_url, message ) = self.helper.check_if_authorized(shib_dct)
+        self.assertEqual( False, is_authorized )
+        self.assertEqual( '/borrow/message/', redirect_url )
+        self.assertTrue( 'you are not authorized' in message )
+
+    def test_check_if_authorized__good_data(self):
+        """ Good data should return True. """
+        shib_dct = { 'member_of': app_settings.REQUIRED_GROUPER_GROUP }
+        ( is_authorized, redirect_url, message ) = self.helper.check_if_authorized(shib_dct)
+        self.assertEqual( True, is_authorized )
+        self.assertEqual( '', redirect_url )
+        self.assertEqual( '', message )
+
     def test_save_to_easyborrow(self):
-        """ Bad data. """
+        """ Bad data should return None. """
         bib_dct = {}
         shib_dct = {}
         querystring = ''
         self.assertEqual( None, self.helper.save_to_easyborrow(shib_dct, bib_dct, querystring) )  # a good save would return the ezb-db-id
 
     # end ProcessHelperTest()
-
