111c1081edd116c1d9cbc6dde7b5883ff1a2693d
birkin <birkin.diana@gmail.com>
2016-06-23 14:12:15 -0400

    removes unused file.

--- clear_sessions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#clear django_session table
-cd ..
-DJANGO_SETTINGS_MODULE="easy_article.settings" \
-python -c 'from django.contrib.sessions.models import Session; \
-      Session.objects.all().delete()' 
-
