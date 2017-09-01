cb726eeb1a16f5dc7280b3fafa338c0b1f31aeb7
birkin <birkin.diana@gmail.com>
2017-09-01 16:01:33 -0400

    flake8s.

--- findit/admin.py
+++ findit/admin.py
@@ -3,8 +3,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import PrintTitle, Request,\
-                   UserMessage
+from models import PrintTitle, Request, UserMessage
 
 
 class RequestAdmin(admin.ModelAdmin):
@@ -13,10 +12,12 @@ class RequestAdmin(admin.ModelAdmin):
     list_filter = ['date_created']
     pass
 
-#class UserProfileAdmin(admin.ModelAdmin):
+
+# class UserProfileAdmin(admin.ModelAdmin):
 #    search_fields = ['membership', 'barcode']
 #    list_display = ['user', 'membership', 'date_created']
 #    pass
+
 
 class PrintTitleAdmin(admin.ModelAdmin):
     search_fields = ['issn', 'call_number', 'location', 'start', 'end', 'call_number']
@@ -24,12 +25,14 @@ class PrintTitleAdmin(admin.ModelAdmin):
     list_filter = ['location']
     pass
 
+
 class UserMessageAdmin(admin.ModelAdmin):
     search_fields = ['type', 'subject', 'text']
     list_display = ['type', 'subject']
-    readonly_fields=('date_created', 'date_modified')
+    readonly_fields = ('date_created', 'date_modified')
+
 
 admin.site.register(Request, RequestAdmin)
-#admin.site.register(UserProfile, UserProfileAdmin)
+# admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(PrintTitle, PrintTitleAdmin)
 admin.site.register(UserMessage, UserMessageAdmin)
