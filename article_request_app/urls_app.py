3a6e970804bb064949ce9e67471a17555153d2d9
birkin <birkin.diana@gmail.com>
2017-09-18 13:52:02 -0400

    flake8 improvements.

--- article_request_app/urls_app.py
+++ article_request_app/urls_app.py
@@ -2,28 +2,29 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
+from django.conf.urls import patterns, url
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
 
-    url( r'^shib_login/$',  'article_request_app.views.shib_login', name='shib_login_url' ),
-    # after shib-login; redirects to `login_handler`.
+    '',
 
-    url( r'^login_handler/$',  'article_request_app.views.login_handler', name='login_handler_url' ),
-    # ensures user comes from correct 'findit' url; checks illiad for new-user or blocked; if happy, redirects to `illiad`, otherwise to `message` with error.
+    url( r'^shib_login/$', 'article_request_app.views.shib_login', name='shib_login_url' ),
+    ## after shib-login; redirects to `login_handler`.
 
-    url( r'^illiad/$',  'article_request_app.views.illiad_request', name='illiad_request_url' ),
-    # displays confirmation 'Submit' button and citation; submission hits 'illiad_handler'
+    url( r'^login_handler/$', 'article_request_app.views.login_handler', name='login_handler_url' ),
+    ## ensures user comes from correct 'findit' url; checks illiad for new-user or blocked; if happy, redirects to `illiad`, otherwise to `message` with error.
 
-    url( r'^illiad_handler/$',  'article_request_app.views.illiad_handler', name='illiad_handler_url' ),
-    # behind-the-scenes, submits request to illiad, then redirects to message_url
+    url( r'^illiad/$', 'article_request_app.views.illiad_request', name='illiad_request_url' ),
+    ## displays confirmation 'Submit' button and citation; submission hits 'illiad_handler'
 
-    url( r'^shib_logout/$',  'article_request_app.views.shib_logout', name=u'shib_logout_url' ),
-    # after shib_logout, redirects to 'message'
+    url( r'^illiad_handler/$', 'article_request_app.views.illiad_handler', name='illiad_handler_url' ),
+    ## behind-the-scenes, submits request to illiad, then redirects to message_url
 
-    url( r'^message/$',  'article_request_app.views.message', name='message_url' ),
-    # shows confirmation or problem message
+    url( r'^shib_logout/$', 'article_request_app.views.shib_logout', name=u'shib_logout_url' ),
+    ## after shib_logout, redirects to 'message'
 
-    )
+    url( r'^message/$', 'article_request_app.views.message', name='message_url' ),
+    ## shows confirmation or problem message
+
+)
