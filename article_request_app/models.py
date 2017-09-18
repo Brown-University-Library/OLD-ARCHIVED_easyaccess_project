17345a1c0fb9f3c90776f7e6b0fb7bc5196bb88f
birkin <birkin.diana@gmail.com>
2017-09-18 13:51:23 -0400

    flake8 improvements.

--- article_request_app/models.py
+++ article_request_app/models.py
@@ -1,11 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-
-import datetime, json, logging, os, pprint, subprocess, tempfile, urllib, urlparse
-import requests
-from . import settings_app
-
-
-log = logging.getLogger(__name__)
-
