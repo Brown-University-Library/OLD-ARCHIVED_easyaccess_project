e2824b41afd4579cb624b4d8c5a01504f7c10152
birkin <birkin.diana@gmail.com>
2017-09-22 11:50:55 -0400

    removes unused code.

--- article_request_app/views.py
+++ article_request_app/views.py
@@ -55,51 +55,6 @@ def shib_login( request ):
     ## end def shib_login()
 
 
-# def shib_login( request ):
-#     """ Builds the SP login and target-return url; redirects to the SP login, which then lands back at the login_handler() url.
-#         Called when views.availability() returns a Request button that's clicked.
-#         Session cleared and info put in url due to revproxy resetting session. """
-#     log_id = request.session.get( 'log_id', '' )
-#     log.debug( '`{id}` article_request shib_login() starting session.items(), ```{val}```'.format(id=log_id, val=pprint.pformat(request.session.items())) )
-
-#     ## store vars we're gonna need
-#     citation_json = request.session.get( 'citation_json', '{}' )
-#     format = request.session.get( 'format', '' )
-#     illiad_url = request.session.get( 'illiad_url', '' )
-#     querystring = request.META.get('QUERY_STRING', '').decode('utf-8')
-
-#     ## clear session so non-rev and rev work same way
-#     for key in request.session.keys():
-#         del request.session[key]
-
-#     ## build login-handler url, whether it's direct (localdev), or indircect-via-shib
-#     # login_handler_querystring = 'citation_json={ctn_jsn}&format={fmt}&illiad_url={ill_url}&querystring={qs}'.format(
-#     #     ctn_jsn=urlquote(citation_json), fmt=urlquote(format), ill_url=urlquote(illiad_url), qs=urlquote(querystring)
-#     #     )
-#     login_handler_querystring = 'citation_json={ctn_jsn}&format={fmt}&illiad_url={ill_url}&querystring={qs}&ezlogid={id}'.format(
-#         ctn_jsn=urlquote(citation_json), fmt=urlquote(format), ill_url=urlquote(illiad_url), qs=urlquote(querystring), id=log_id
-#         )
-#     login_handler_url = '{scheme}://{host}{login_handler_url}?{querystring}'.format(
-#         scheme=request.scheme, host=request.get_host(), login_handler_url=reverse('article_request:login_handler_url'), querystring=login_handler_querystring )
-#     log.debug( 'pre-encoded login_handler_url, ```{}```'.format(login_handler_url) )
-
-#     ## return response
-#     localdev_check = False
-#     if request.get_host() == '127.0.0.1' and project_settings.DEBUG2 == True:  # eases local development
-#         localdev_check = True
-#     if localdev_check is True:
-#         log.debug( 'localdev_check is True, redirecting right to pre-encoded login_handler' )
-#         return HttpResponseRedirect( login_handler_url )
-#     else:
-#         encoded_login_handler_url = urlquote( login_handler_url )
-#         redirect_url = '{shib_login}?target={encoded_login_handler_url}'.format(
-#             shib_login=settings_app.SHIB_LOGIN_URL, encoded_login_handler_url=encoded_login_handler_url )
-#         log.debug( 'redirect_url to shib-sp-login, ```{}```'.format(redirect_url) )
-#         return HttpResponseRedirect( redirect_url )
-
-#     ## end def shib_login()
-
-
 def login_handler( request ):
     """ Ensures user comes from correct 'findit' url;
         then grabs shib info;
@@ -145,11 +100,6 @@ def login_handler( request ):
         request.session['message'] = message
         return HttpResponseRedirect( redirect_url )
 
-    # ## log user into illiad
-    # ( illiad_instance, success ) = ill_helper.login_user( request, shib_dct )
-    # if success is False:
-    #     return HttpResponseRedirect( reverse('article_request:message_url') )  # handles blocked or failed-user-registration problems
-
     ## log user into illiad
     citation_json = request.session.get( 'citation_json', '{}' )
     citation_dct = json.loads( citation_json )
@@ -157,7 +107,6 @@ def login_handler( request ):
         title = citation_dct['title']
     else:
         title = citation_dct.get('source', 'title_unavailable')
-
     login_result_dct = new_ill_helper.login_user( shib_dct, title )
     if login_result_dct['success'] is not True:
         return HttpResponseRedirect( reverse('article_request:message_url') )  # handles blocked or failed-user-registration problems
@@ -308,7 +257,7 @@ def illiad_handler( request ):
     ## redirect
     return HttpResponseRedirect( redirect_url )
 
-    # end def illiad_handler()
+    ## end def illiad_handler()
 
 
 def shib_logout( request ):
