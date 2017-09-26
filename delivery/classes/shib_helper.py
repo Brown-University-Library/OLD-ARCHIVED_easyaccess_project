23b68d99bba17f0173342353eea82f78fabded9d
birkin <birkin.diana@gmail.com>
2017-09-26 15:34:16 -0400

    stubs new login test and defs.

---
+++ delivery/classes/shib_helper.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import logging
+from article_request_app import settings_app
+from django.utils.http import urlencode, urlquote
+
+
+log = logging.getLogger('access')
+
+
+class ShibLoginHelper( object ):
+    """ Contains helpers for views.login_handler() """
+
+    def build_shib_sp_querystring( self, citation_json, format, illiad_url, querystring, log_id ):
+        """ Builds querystring for redirect to shib SP url, which will redirect back to views.login_handler().
+            Called by views.shib_login() """
+        self.check_params( [ citation_json, format, illiad_url, querystring, log_id ] )
+        segment = '/easyaccess/article_request/login_handler/?citation_json={ctn_jsn}&format={fmt}&illiad_url={ill_url}&querystring={qs}&ezlogid={id}'.format(
+            ctn_jsn=urlquote(citation_json),
+            fmt=urlquote(format),
+            ill_url=urlquote(illiad_url),
+            qs=urlquote(querystring),
+            id=log_id )
+        querystring = urlencode( {'target': segment} ).decode( 'utf-8' )  # yields 'target=(encoded-segment)'
+        assert type(querystring) == unicode, type(querystring)
+        log.debug( 'querystring for redirect to shib SP login url, ```%s```' % querystring )
+        return querystring
+
+    def build_localdev_querystring( self, citation_json, format, illiad_url, querystring, log_id ):
+        """ Builds querystring for redirect right to views.login_handler()
+            Called by views.shib_login() """
+        self.check_params( [ citation_json, format, illiad_url, querystring, log_id ] )
+        querystring = 'citation_json={ctn_jsn}&format={fmt}&illiad_url={ill_url}&querystring={qs}&ezlogid={id}'.format(
+            ctn_jsn=urlquote(citation_json),
+            fmt=urlquote(format),
+            ill_url=urlquote(illiad_url),
+            qs=urlquote(querystring),
+            id=log_id )
+        assert type(querystring) == unicode, type(querystring)
+        log.debug( 'querystring for localdev redirect to views.login_handler(), ```%s```' % querystring )
+        return querystring
+
+    def check_params(self, params_list ):
+        """ Checks that params are unicode-strings.
+            Called by build_localdev_querystring() and build_shib_sp_querystring() """
+        for param in params_list:
+            assert type(param) == unicode, type(param)
+        return
+
+    ## end class ShibLoginHelper()
---
+++ delivery/tests/test_shib_helper.py
@@ -0,0 +1,43 @@
+
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import logging, pprint
+from delivery.classes.shib_helper import ShibLoginHelper
+from django.test import TestCase
+
+
+log = logging.getLogger('access')
+# TestCase.maxDiff = None
+
+
+class ShibLoginHelperTest( TestCase ):
+    """ Tests querystring builder called from views.shib_login() """
+
+    def setUp(self):
+        self.helper = ShibLoginHelper()
+
+    def test_build_shib_sp_querystring(self):
+        """ Checks localdev querystring. """
+        bib_dct_json = '{"param_a": "a\\u00e1a"}'  # json version of {'param_a': 'aáa'}
+        last_querystring = 'rft.atitle=Stalking the Wild Basenji'
+        permalink_url = 'http://domain/aa/bb/shortlink_letters/'
+        ezlogid = 'foo'
+        self.assertEqual(
+            'target=%2Feasyaccess%2Farticle_request%2Flogin_handler%2F%3Fcitation_json%3D%257B%2522param_a%2522%253A%2520%2522a%255Cu00e1a%2522%257D%26format%3Djournal%26illiad_url%3Dhttps%253A%2F%2Fdomain%2Faa%2Fbb%2FOpenURL%253Frft.atitle%253DStalking%2520the%2520Wild%2520Basenji%26querystring%3Drft.atitle%253DStalking%2520the%2520Wild%2520Basenji%26ezlogid%3Dfoo',
+            self.helper.build_shib_sp_querystring( bib_dct_json, last_querystring, permalink_url, ezlogid )
+        )
+
+    def test_build_localdev_querystring(self):
+        """ Checks localdev querystring. """
+        bib_dct_json = '{"param_a": "a\\u00e1a"}'  # json version of {'param_a': 'aáa'}
+        last_querystring = 'rft.atitle=Stalking the Wild Basenji'
+        permalink_url = 'http://domain/aa/bb/shortlink_letters/'
+        ezlogid = 'foo'
+        self.assertEqual(
+            'citation_json=%7B%22param_a%22%3A%20%22a%5Cu00e1a%22%7D&format=journal&illiad_url=https%3A//domain/aa/bb/OpenURL%3Frft.atitle%3DStalking%20the%20Wild%20Basenji&querystring=rft.atitle%3DStalking%20the%20Wild%20Basenji&ezlogid=foo',
+            self.helper.build_localdev_querystring( bib_dct_json, last_querystring, permalink_url, ezlogid )
+        )
+
+    ## end class ShibLoginHelperTest()
