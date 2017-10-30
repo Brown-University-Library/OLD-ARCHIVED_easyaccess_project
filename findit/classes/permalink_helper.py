1f07e404f32bc5783e09bfe07ab58fed88c0bc03
birkin <birkin.diana@gmail.com>
2017-10-30 15:59:31 -0400

    refactors shortlink creation.

--- findit/classes/permalink_helper.py
+++ findit/classes/permalink_helper.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import logging, pprint
 
 from delivery.models import Resource
+from django.core.urlresolvers import reverse
 from shorturls import baseconv
 
 
@@ -15,12 +16,22 @@ class Permalink( object ):
     """ Handles creation of permalinks from views.findit_base_resolver(),
         and lookup from incoming permalinks from views.permalink() """
 
+    # def make_permalink( self, referrer, qstring, scheme, host, path ):
+    #     """ Creates a delivery.models.Resource entry if one doesn't exist, and creates and returns a permalink string.
+    #         Called by views.base_resolver() """
+    #     resource_id = self._get_resource( qstring, referrer )
+    #     permastring = baseconv.base62.from_decimal( resource_id )
+    #     permalink = '%s://%s%spermalink/%s/' % ( scheme, host, path, permastring )
+    #     return_dct = { 'permalink': permalink, 'querystring': qstring, 'referrer': referrer, 'resource_id': resource_id, 'permastring': permastring  }
+    #     log.debug( 'return_dct, ```%s```' % pprint.pformat(return_dct) )
+    #     return return_dct
+
     def make_permalink( self, referrer, qstring, scheme, host, path ):
         """ Creates a delivery.models.Resource entry if one doesn't exist, and creates and returns a permalink string.
             Called by views.base_resolver() """
         resource_id = self._get_resource( qstring, referrer )
         permastring = baseconv.base62.from_decimal( resource_id )
-        permalink = '%s://%s%spermalink/%s/' % ( scheme, host, path, permastring )
+        permalink = reverse( 'findit:permalink_url', kwargs={'permalink_str': permastring} )
         return_dct = { 'permalink': permalink, 'querystring': qstring, 'referrer': referrer, 'resource_id': resource_id, 'permastring': permastring  }
         log.debug( 'return_dct, ```%s```' % pprint.pformat(return_dct) )
         return return_dct
