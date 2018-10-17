1599e46851ade900e259d99e3ba7752d36aebdaa
birkin <birkin.diana@gmail.com>
2018-10-17 13:41:33 -0400

    updates var name.

--- findit/views.py
+++ findit/views.py
@@ -265,6 +265,6 @@ def info( request ):
 def bul_search( request ):
     """ Triggered by user entering search term into banner-search-field.
         Redirects query to search.library.brown.edu """
-    log.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
+    alog.debug( 'request.__dict__, ```%s```' % pprint.pformat(request.__dict__) )
     redirect_url = 'https://search.library.brown.edu?%s' % request.META['QUERY_STRING']
     return HttpResponseRedirect( redirect_url )
