5db36d2766399679919fc341bcc8ef6aa306f488
birkin <birkin.diana@gmail.com>
2019-07-18 08:41:14 -0400

    modernizes wsgi config.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -1,52 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
 """
-Prepares application environment.
+WSGI config.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-Variables assume project setup like:
-stuff
-    easyaccess_project
-        config
-        delivery
-        findit
-    env_eza
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+"""
+Note: no need to activate the virtual-environment here for passenger.
+- the project's httpd/passenger.conf section allows specification of the python-path via `PassengerPython`, which auto-activates it.
+- the auto-activation provides access to modules, but not, automatically, env-vars.
+- passenger env-vars loading under python3.x is enabled via the `SenEnv` entry in the project's httpd/passenger.conf section.
+  - usage: `SetEnv BUL_CBP__SETTINGS_PATH /path/to/BUL_CBP__env_settings.sh`
+  - `SenEnv` requires apache env_module; info: <https://www.phusionpassenger.com/library/indepth/environment_variables.html>,
+     enabled by default on macOS 10.12.4, and our dev and production servers.
+
+For activating the virtual-environment manually, don't source the settings file directly. Instead, add to `project_env/bin/activate`:
+  export BUL_CBP__SETTINGS_PATH="/path/to/BUL_CBP__env_settings.sh"
+  source $BUL_CBP__SETTINGS_PATH
+This allows not only the sourcing, but also creates the env-var used below by shellvars.
 """
 
 import os, pprint, sys
+import shellvars
+from django.core.wsgi import get_wsgi_application
 
 
-## become self-aware, padawan
-current_directory = os.path.dirname(os.path.abspath(__file__))
+# print( 'the initial env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
-## vars
-ACTIVATE_FILE = os.path.abspath( u'%s/../../env_eza/bin/activate_this.py' % current_directory )
-PROJECT_DIR = os.path.abspath( u'%s/../../easyaccess_project' % current_directory )
-PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
-SETTINGS_MODULE = u'config.settings'
-SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_eza/lib/python2.7/site-packages' % current_directory )
+PROJECT_DIR_PATH = os.path.dirname( os.path.dirname(os.path.abspath(__file__)) )
+ENV_SETTINGS_FILE = os.environ['EZACS__SETTINGS_PATH']  # set in `httpd/passenger.conf`, and `env/bin/activate`
 
-## virtualenv
-execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
+## update path
+sys.path.append( PROJECT_DIR_PATH )
 
-## sys.path additions
-for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
- if entry not in sys.path:
-   sys.path.append( entry )
-
-## environment additions
-os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
+## reference django settings
+os.environ[u'DJANGO_SETTINGS_MODULE'] = 'config.settings'  # so django can access its settings
 
 ## load up env vars
-SETTINGS_FILE = os.environ['EZACS2015__SETTINGS_PATH']  # set in activate_this.py, and activated above
-import shellvars
-var_dct = shellvars.get_vars( SETTINGS_FILE )
+var_dct = shellvars.get_vars( ENV_SETTINGS_FILE )
 for ( key, val ) in var_dct.items():
-    os.environ[key] = val
+    print(f'type(key), `{type(key)}`; type(val), `{type(val)}`' )
+    os.environ[key.decode('utf-8')] = val.decode('utf-8')
+
+# print( 'the final env, ```{}```'.format( pprint.pformat(dict(os.environ)) ) )
 
 ## gogogo
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
