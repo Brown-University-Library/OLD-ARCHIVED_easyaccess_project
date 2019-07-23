5d9ba10bf57629f5aaf4453a3393783c4526666b
birkin <birkin.diana@gmail.com>
2019-07-23 11:05:35 -0400

    updates parse_qs() usage for py3.

--- findit/tests/test_utils.py
+++ findit/tests/test_utils.py
@@ -2,7 +2,8 @@
 
 from __future__ import unicode_literals
 
-import logging, pprint, unittest, urllib, urlparse
+import logging, pprint, unittest, urllib
+from urllib.parse import parse_qs
 
 from django.conf import settings
 # from django.utils.log import dictConfig
@@ -28,7 +29,7 @@ class IlliadURLTests(unittest.TestCase):
         ourl = 'sid=FirstSearch:WorldCat&title=Einstein on the beach&date=1993&aulast=Glass&aufirst=Philip&id=doi:&pid=<accession number>29050194</accession number><fssessid>0</fssessid>&url_ver=Z39.88-2004&rfr_id=info:sid/firstsearch.oclc.org:WorldCat&rft_val_fmt=info:ofi/fmt:kev:mtx:book&req_dat=<sessionid>0</sessionid>&rfe_dat=<accessionnumber>29050194</accessionnumber>&rft_id=info:oclcnum/29050194&rft.aulast=Glass&rft.aufirst=Philip&rft.title=Einstein on the beach&rft.date=1993&rft.aucorp=Philip Glass Ensemble.&rft.place=New York, NY :&rft.pub=Elektra Nonesuch,&rft.genre=unknown'
         ourl = 'rfr_id%3DFirstSearch%253AWorldCat%26isbn%3D9780767853569%26title%3DA%2Bfew%2Bgood%2Bmen%26date%3D2001%26aulast%3DReiner%26aufirst%3DRob%26id%3Ddoi%253A%26pid%3D%253Caccession%2Bnumber%253E47247051%253C%252Faccession%2Bnumber%253E%253Cfssessid%253E0%253C%252Ffssessid%253E%26url_ver%3DZ39.88-2004%26rfr_id%3Dinfo%253Asid%252Ffirstsearch.oclc.org%253AWorldCat%26rft_val_fmt%3Dinfo%253Aofi%252Ffmt%253Akev%253Amtx%253Abook%26req_dat%3D%253Csessionid%253E0%253C%252Fsessionid%253E%26rfe_dat%3D%253Caccessionnumber%253E47247051%253C%252Faccessionnumber%253E%26rft_id%3Dinfo%253Aoclcnum%252F47247051%26rft_id%3Durn%253AISBN%253A9780767853569%26rft.aulast%3DReiner%26rft.aufirst%3DRob%26rft.title%3DA%2Bfew%2Bgood%2Bmen%26rft.date%3D2001%26rft.isbn%3D9780767853569%26rft.aucorp%3DColumbia%2BPictures.%253BCastle%2BRock%2BEntertainment%2B%2528Firm%2529%253BColumbia%2BTriStar%2BHome%2BVideo%2B%2528Firm%2529%26rft.place%3DCulver%2BCity%252C%2BCA%2B%253A%26rft.pub%3DColumbia%2BTriStar%2BHome%2BVideo%252C%26rft.genre%3Dunknown%26checksum%3D065b7cc8e6abf86e89b5639ee3939c09&title=Brown+University&linktype=openurl&detail=RBN'
         ill = make_illiad_url(ourl)
-        ill_dict = urlparse.parse_qs(ill)
+        ill_dict = parse_qs(ill)
         #pprint(ill_dict)
         self.assertTrue('FirstSearch:WorldCat' in ill_dict['sid'])
 
@@ -38,7 +39,7 @@ class IlliadURLTests(unittest.TestCase):
         orig_ourl = 'rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft.auinitm=D&rft.spage=392&rft.au=Schlenker%2C+J+D&rft.aulast=Schlenker&sid=bul_easy_article&rft_id=info%3Apmid%2F7252116&rft.date=1981-07&rft.issn=0363-5023&rft.aufirst=J&rft.volume=6&version=1.0&url_ver=Z39.88-2004&rft.atitle=Three+complications+of+untreated+partial+laceration+of+flexor+tendon--entrapment%2C+rupture%2C+and+triggering&rft.eissn=1531-6564&pmid=7252116&rft.jtitle=The+Journal+of+hand+surgery+%28American+ed.%29&rft.issue=4&rft.genre=article'
         ill_ourl = make_illiad_url( orig_ourl )
         # ilog.debug( 'ill_ourl, `%s`' % ill_ourl )
-        ill_ourl_dct = urlparse.parse_qs( ill_ourl )  # all dct values are lists
+        ill_ourl_dct = parse_qs( ill_ourl )  # all dct values are lists
         # ilog.debug( 'ill_ourl_dct, `%s`' % pprint.pformat(ill_ourl_dct) )
         date = ill_ourl_dct['rft.date'][0]
         self.assertEqual( '1981-07', date )
@@ -49,7 +50,7 @@ class IlliadURLTests(unittest.TestCase):
         """
         ourl = 'rft.eissn=1531-6564&rft.aulast=Schlenker&rft.au=Schlenker%2C+J+D&rft.atitle=Three+complications+of+untreated+partial+laceration+of+flexor+tendon--entrapment%2C+rupture%2C+and+triggering&rft.volume=6&rft.jtitle=The+Journal+of+hand+surgery+%28American+ed.%29&rft.aufirst=J&rft.date=1981-07&rft.auinitm=D&rft.spage=392&rft.issue=4&pmid=7252116&rft_id=info%3Apmid%2F7252116&rft.issn=0363-5023&url_ver=Z39.88-2004&version=1.0&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft.genre=article'
         ill = make_illiad_url(ourl)
-        ill_dict = urlparse.parse_qs(ill)
+        ill_dict = parse_qs(ill)
         note = ill_dict['Notes'][0]
         self.assertTrue(note.rfind('7252116') > -1)
 
