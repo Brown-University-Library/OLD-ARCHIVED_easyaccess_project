d07205ccb71970a161d9e127c1de680f3bb50182
birkin <birkin.diana@gmail.com>
2019-09-23 14:03:59 -0400

    adds illiad-enhance test.

--- article_request_app/tests.py
+++ article_request_app/tests.py
@@ -53,8 +53,8 @@ class IlliadUrlBuilderTest( TestCase ):
         # self.host = 'dummy_host'
         # self.permalink = 'dummy_permalink'
 
-    def test_enhance_citation(self):
-        """ Checks that poor bib-dct data is enhanced when possible. """
+    def test_enhance_citation_A(self):
+        """ Checks that poor bib-dct author data is enhanced with `rft.creator` data if possible. """
         querystring = 'rft.creator=Yu%2C+Xingfeng&rft.source=Effectiveness+of+a+Nurse-Led+Integrative+Health+and+Wellness+%28NIHaW%29+Programme+on+Behavioural%2C+Psychosocial+and+Biomedical+Outcomes+among+Individuals+with+Newly+Diagnosed+Type+2+Diabetes%3A+A+Randomised+Controlled+Trial&rft.date=2018-01-01&rft.creatorFirst=Xingfeng&rft.creatorLast=Yu&rft.isbn=9780438852358&url_ver=Z39.88-2004&version=1.0&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft.genre=article&rfr_id=info%3Asid%2FProQuest+Dissertations+%26+Theses+Global'
         initial_bib_dct = {'_openurl': 'ctx_ver=Z39.88-2004&rft_val_fmt=info%3Aofi/fmt%3Akev%3Amtx%3Ajournal&rft.atitle=Unknown&rft.genre=article&rfr_id=info%3Asid/info%3Asid/ProQuest+Dissertations+%26+Theses+Global&rft.date=2018&rft.isbn=9780438852358',
              '_rfr': 'info:sid/ProQuest Dissertations & Theses Global',
@@ -65,6 +65,28 @@ class IlliadUrlBuilderTest( TestCase ):
              'title': 'Unknown',
              'type': 'article',
              'year': '2018'}
+        returned_bib_dct = self.builder.enhance_citation( initial_bib_dct, querystring )
+        log.debug( f'returned_bib_dct, ```{pprint.pformat(returned_bib_dct)}```' )
+        self.assertEqual(
+            [ {'name': '(?) Yu, Xingfeng'} ],
+            returned_bib_dct['author'] )
+        self.assertEqual(
+            '(?) Effectiveness of a Nurse-Led Integrative Health and Wellness (NIHaW) Programme on Behavioural, Psychosocial and Biomedical Outcomes among Individuals with Newly Diagnosed Type 2 Diabetes: A Randomised Controlled Trial',
+            returned_bib_dct['title'] )
+
+    def test_enhance_citation_B(self):
+        """ Checks that poor bib-dct author data is enhanced with `rft.au` data if possible. """
+        querystring = 'ctx_ver=Z39.88-2004&ctx_enc=info:ofi/enc:UTF-8&rfr_id=info:sid/ProQuest+Dissertations+%26+Theses+Global&rft_val_fmt=info:ofi/fmt:kev:mtx:dissertation&rft.genre=dissertations+%26+theses&rft.jtitle=&rft.atitle=&rft.au=Yu%2C+Xingfeng&rft.aulast=Yu&rft.aufirst=Xingfeng&rft.date=2018-01-01&rft.volume=&rft.issue=&rft.spage=&rft.isbn=9780438852358&rft.btitle=&rft.title=Effectiveness+of+a+Nurse-Led+Integrative+Health+and+Wellness+%28NIHaW%29+Programme+on+Behavioural%2C+Psychosocial+and+Biomedical+Outcomes+among+Individuals+with+Newly+Diagnosed+Type+2+Diabetes%3A+A+Randomised+Controlled+Trial&rft.issn=&rft_id=info:doi/'
+        initial_bib_dct = {
+            '_openurl': 'ctx_ver=Z39.88-2004&rft_val_fmt=info%3Aofi/fmt%3Akev%3Amtx%3Ajournal&rft.atitle=Unknown&rft.genre=article&rfr_id=info%3Asid/info%3Asid/ProQuest+Dissertations+%26+Theses+Global&rft.date=2018&rft.isbn=9780438852358',
+            '_rfr': 'info:sid/ProQuest Dissertations & Theses Global',
+            '_valid': False,
+            'identifier': [{'id': '9780438852358', 'type': 'isbn'}],
+            'journal': {'name': 'Not provided'},
+            'pages': '? - ?',
+            'title': 'Unknown',
+            'type': 'article',
+            'year': '2018' }
         returned_bib_dct = self.builder.enhance_citation( initial_bib_dct, querystring )
         log.debug( f'returned_bib_dct, ```{pprint.pformat(returned_bib_dct)}```' )
         self.assertEqual(
